c5b6c7fca8bf170620c2cc5b263144196ce958ac
mac718 <mac718@gmail.com>
2018-02-13 18:32:32 -0500

    Add edit and update controllers and edit view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,6 +14,20 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update 
+    @user = User.find(params[:id])
+
+    if @user.save
+      redirect_to new_user_path
+    else
+      render :edit
+    end
+  end
+
   def user_params
     params.require(:user).permit(:username, :email, :password)
   end
